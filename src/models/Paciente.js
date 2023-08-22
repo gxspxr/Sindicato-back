6dce8d03896a39a839d548107158306b707b7f86
Gxspxr <sernagas1234@gmail.com>
2023-08-22 15:39:07 -0300

    pacient cel range fixed

--- src/models/Paciente.js
+++ src/models/Paciente.js
@@ -18,7 +18,7 @@ module.exports = (sequelize) => {
         allowNull: false
       },
       cel:{
-        type: DataTypes.INTEGER,
+        type: DataTypes.BIGINT,
         allowNull: true
       },
       insurance:{
